62e15de36ed6ab7335ca1a0e733ba9bfb7330fa4
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-01-25 18:57:35 +0800

    fix: two way binding complete

--- src/components/pages/SystemSettings/Role/NewRole/Permissions/All.vue
+++ src/components/pages/SystemSettings/Role/NewRole/Permissions/All.vue
@@ -1,65 +1,77 @@
 <template>
-    <EmulationRange v-model:range="permissionsStatus.range" v-model:container="permissionsStatus.container"
-        v-model:mirror="permissionsStatus.mirror" @update:range="(range) => { permissionsStatus.range = range }"
-        @update:container="(container) => {
-            permissionsStatus.container = container;
-        }" @update:mirror="(mirror) => {
-    permissionsStatus.mirror = mirror;
-}" />
-    <el-checkbox v-model="permissionsStatus.attackDefense" @change="handleChange">攻防演练</el-checkbox>
-    <el-checkbox v-model="permissionsStatus.loopholes">漏洞库管理</el-checkbox>
-    <el-checkbox v-model="permissionsStatus.system">系统管理</el-checkbox>
+  <EmulationRange
+    v-model:range="permissionsStatus.range"
+    v-model:container="permissionsStatus.container"
+    v-model:mirror="permissionsStatus.mirror"
+    @update:range="
+      (range) => {
+        permissionsStatus.range = range;
+      }
+    "
+    @update:container="
+      (container) => {
+        permissionsStatus.container = container;
+      }
+    "
+    @update:mirror="
+      (mirror) => {
+        permissionsStatus.mirror = mirror;
+      }
+    "
+  />
+  <el-checkbox v-model="permissionsStatus.attackDefense">攻防演练</el-checkbox>
+  <el-checkbox v-model="permissionsStatus.loopholes">漏洞库管理</el-checkbox>
+  <el-checkbox v-model="permissionsStatus.system">系统管理</el-checkbox>
 </template>
 
 <script lang="ts" setup>
-import { containsProp } from '@vueuse/core';
-import EmulationRange from './EmulationRange.vue';
-import { ref, defineProps, onBeforeMount } from 'vue';
+import EmulationRange from "./EmulationRange.vue";
+import { ref, defineProps, onBeforeMount } from "vue";
 
 interface AllPermissions {
-    range: boolean,
-    container: boolean,
-    mirror: boolean,
-    attackDefense: boolean,
-    loopholes: boolean,
-    system: boolean,
-};
+  range: boolean;
+  container: boolean;
+  mirror: boolean;
+  attackDefense: boolean;
+  loopholes: boolean;
+  system: boolean;
+}
 
 const props = defineProps<{
-    permissions: string[],
+  permissions: string[];
 }>();
 
 const permissionsStatus = ref<AllPermissions>({
-    range: false,
-    container: false,
-    mirror: false,
-    attackDefense: false,
-    loopholes: false,
-    system: false,
+  range: false,
+  container: false,
+  mirror: false,
+  attackDefense: false,
+  loopholes: false,
+  system: false,
 });
 
 onBeforeMount(() => {
-    if (props.permissions.indexOf('靶场管理') !== -1) {
-        permissionsStatus.value.range = true;
-    }
-    if (props.permissions.indexOf('容器管理') !== -1) {
-        permissionsStatus.value.container = true;
-    }
-    if (props.permissions.indexOf('镜像管理') !== -1) {
-        permissionsStatus.value.mirror = true;
-    }
-    if (props.permissions.indexOf('攻防管理') !== -1) {
-        permissionsStatus.value.attackDefense = true;
-    }
-    if (props.permissions.indexOf('漏洞库管理') !== -1) {
-        permissionsStatus.value.loopholes = true;
-    }
-    if (props.permissions.indexOf('系统管理') !== -1) {
-        permissionsStatus.value.system = true;
-    }
+  if (props.permissions.indexOf("靶场管理") !== -1) {
+    permissionsStatus.value.range = true;
+  }
+  if (props.permissions.indexOf("容器管理") !== -1) {
+    permissionsStatus.value.container = true;
+  }
+  if (props.permissions.indexOf("镜像管理") !== -1) {
+    permissionsStatus.value.mirror = true;
+  }
+  if (props.permissions.indexOf("攻防管理") !== -1) {
+    permissionsStatus.value.attackDefense = true;
+  }
+  if (props.permissions.indexOf("漏洞库管理") !== -1) {
+    permissionsStatus.value.loopholes = true;
+  }
+  if (props.permissions.indexOf("系统管理") !== -1) {
+    permissionsStatus.value.system = true;
+  }
 });
 
 const handleChange = () => {
-    console.log(permissionsStatus.value);
+  console.log(permissionsStatus.value);
 };
 </script>
--- src/components/pages/SystemSettings/Role/NewRole/Permissions/EmulationRange.vue
+++ src/components/pages/SystemSettings/Role/NewRole/Permissions/EmulationRange.vue
@@ -1,75 +1,145 @@
 <template>
-    <el-checkbox v-model="isAll" :indeterminate="isIndeterminate" @change="handleAllChange">仿真靶场管理</el-checkbox>
-    <el-checkbox v-model="permissionRange" @change="updateRange">靶场管理</el-checkbox>
-    <el-checkbox v-model="permissionContainer" @change="updateContainer">容器管理</el-checkbox>
-    <el-checkbox v-model="permissionMirror" @change="updateMirror">镜像管理</el-checkbox>
+  <el-checkbox
+    v-model="isAll"
+    :indeterminate="isIndeterminate"
+    @change="handleAllChange"
+    >仿真靶场管理</el-checkbox
+  >
+  <el-checkbox v-model="permissionRange" @change="updateRange"
+    >靶场管理</el-checkbox
+  >
+  <el-checkbox v-model="permissionContainer" @change="updateContainer"
+    >容器管理</el-checkbox
+  >
+  <el-checkbox v-model="permissionMirror" @change="updateMirror"
+    >镜像管理</el-checkbox
+  >
 </template>
 
 <script lang="ts" setup>
-import { ref, defineProps, defineEmits, onBeforeMount } from 'vue';
-import { CheckboxValueType } from 'element-plus';
+import { ref, defineProps, defineEmits, onBeforeMount } from "vue";
+import { CheckboxValueType } from "element-plus";
 
 const props = defineProps<{
-    range: boolean,
-    container: boolean,
-    mirror: boolean,
+  range: boolean;
+  container: boolean;
+  mirror: boolean;
 }>();
-const emit = defineEmits([
-    'update:range',
-    'update:container',
-    'update:mirror',
-]);
-const updateRange = (value: CheckboxValueType) => {
-    emit('update:range', permissionRange.value);
-    // handleIndeterminateChange();
+const emit = defineEmits(["update:range", "update:container", "update:mirror"]);
+const updateRange = () => {
+  emit("update:range", permissionRange.value);
+  handleIndeterminateChange();
 };
 const updateContainer = () => {
-    emit('update:container', permissionContainer.value);
+  emit("update:container", permissionContainer.value);
+  handleIndeterminateChange();
 };
 const updateMirror = () => {
-    emit('update:mirror', permissionMirror.value);
+  emit("update:mirror", permissionMirror.value);
+  handleIndeterminateChange();
 };
 
 const isAll = ref(false);
 const isIndeterminate = ref(false);
+
+// const options = ref({
+//     range: {
+//         cnName: '靶场管理',
+//         value: false,
+//     },
+//     container: {
+//         cnName: '容器管理',
+//         value: false,
+//     },
+//     mirror: {
+//         cnName: '镜像管理',
+//         value: false,
+//     },
+//     attackDefense: {
+//         cnName: '攻防演练',
+//         value: false,
+//     },
+//     loopholes: {
+//         cnName: '漏洞库管理',
+//         value: false,
+//     },
+//     system: {
+//         cnName: '系统管理',
+//         value: false,
+//     },
+// });
+
 const permissionRange = ref(false);
 const permissionContainer = ref(false);
 const permissionMirror = ref(false);
+
 const checkedOptions = ref([] as string[]);
-const options: string[] = ['靶场管理', '容器管理', '镜像管理'];
+const allOptions: string[] = ["靶场管理", "容器管理", "镜像管理"];
+
+const clearCheckedOptions = () => {
+  checkedOptions.value = checkedOptions.value.filter((option) => {
+    checkedOptions.value.indexOf(option) === -1;
+  });
+};
+const updateArrayBasedOnTruthValues = () => {
+  clearCheckedOptions();
+  if (permissionRange.value) {
+    checkedOptions.value.push("靶场管理");
+  }
+  if (permissionContainer.value) {
+    checkedOptions.value.push("容器管理");
+  }
+  if (permissionMirror.value) {
+    checkedOptions.value.push("镜像管理");
+  }
+};
+const updateTruthValuesBasedOnArray = () => {
+  permissionRange.value = false;
+  permissionContainer.value = false;
+  permissionMirror.value = false;
+
+  if (checkedOptions.value.indexOf("靶场管理") !== -1) {
+    permissionRange.value = true;
+  }
+  if (checkedOptions.value.indexOf("容器管理") !== -1) {
+    permissionContainer.value = true;
+  }
+  if (checkedOptions.value.indexOf("镜像管理") !== -1) {
+    permissionMirror.value = true;
+  }
+};
 
 onBeforeMount(() => {
-    if (props.range) permissionRange.value = true;
-    if (props.container) permissionContainer.value = true;
-    if (props.mirror) permissionMirror.value = true;
+  if (props.range) permissionRange.value = true;
+  if (props.container) permissionContainer.value = true;
+  if (props.mirror) permissionMirror.value = true;
 
-    if (props.range)
-        checkedOptions.value.push('靶场管理');
-    if (props.container)
-        checkedOptions.value.push('容器管理');
-    if (props.mirror)
-        checkedOptions.value.push('镜像管理');
+  if (props.range) checkedOptions.value.push("靶场管理");
+  if (props.container) checkedOptions.value.push("容器管理");
+  if (props.mirror) checkedOptions.value.push("镜像管理");
 
-    if (checkedOptions.value.length === options.length) {
-        isAll.value = true;
-        isIndeterminate.value = false;
-    } else if (checkedOptions.value.length === 0) {
-        isAll.value = false;
-        isIndeterminate.value = false;
-    } else {
-        isAll.value = false;
-        isIndeterminate.value = true;
-    }
+  if (checkedOptions.value.length === allOptions.length) {
+    isAll.value = true;
+    isIndeterminate.value = false;
+  } else if (checkedOptions.value.length === 0) {
+    isAll.value = false;
+    isIndeterminate.value = false;
+  } else {
+    isAll.value = false;
+    isIndeterminate.value = true;
+  }
 });
 
 const handleAllChange = (val: CheckboxValueType) => {
-    checkedOptions.value = val ? options : [] as string[];
-    isIndeterminate.value = false;
-}
-const handleIndeterminateChange = (value: CheckboxValueType[]) => {
-    const checkedCount = value.length;
-    const allCount = options.length;
-    isAll.value = checkedCount === allCount;
-    isIndeterminate.value = checkedCount > 0 && checkedCount < allCount;
-}
+  checkedOptions.value = val ? allOptions : ([] as string[]);
+  isIndeterminate.value = false;
+  updateTruthValuesBasedOnArray();
+};
+const handleIndeterminateChange = () => {
+  updateArrayBasedOnTruthValues();
+  const checkedCount = checkedOptions.value.length;
+  const allCount = allOptions.length;
+  isAll.value = checkedCount === allCount;
+  isIndeterminate.value = checkedCount > 0 && checkedCount < allCount;
+};
 </script>
