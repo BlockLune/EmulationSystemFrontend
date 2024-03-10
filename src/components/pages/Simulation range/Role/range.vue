dafc49e3884e832f85ba8c2cf6ae12856e6ca287
haozihuilaile <2193923488@qq.com>
2024-03-10 21:19:31 +0800

    update

--- src/components/pages/Simulation range/Role/range.vue
+++ src/components/pages/Simulation range/Role/range.vue
@@ -38,62 +38,17 @@
   
     <!-- 表格主体 -->
     <el-table :data="users" stripe style="width: 100%">
-      <el-table-column prop="username" label="镜像名称"></el-table-column>
-      <el-table-column prop="role" label="版本名称"></el-table-column>
-      <el-table-column prop="loginName" label="镜像类型"></el-table-column>
-      <el-table-column prop="lastLoginAt" label="上传时间"></el-table-column>
-      <el-table-column prop="lastModifyAt" label="修改时间"></el-table-column>
-      <el-table-column label="操作">
-        <template #default="{ row }">
-          <el-button link type="primary" @click="showEditDialog(row)"
-            >更新</el-button
-          >
-          <el-popconfirm
-            title="确认删除？"
-            confirm-button-text="确认"
-            cancel-button-text="取消"
-            @confirm="deleteRow(row)"
-          >
-            <template #reference>
-              <el-button link type="danger">删除</el-button>
-            </template>
-          </el-popconfirm>
-        </template>
-      </el-table-column>
+      <el-table-column prop="range name" label="靶场名称"></el-table-column>
+      <el-table-column prop="attack&defence name" label="攻防演练名称"></el-table-column>
+      <el-table-column prop="range start time" label="靶场启动时间"></el-table-column>
+      <el-table-column prop="range end time" label="靶场终止时间"></el-table-column>
+      <el-table-column prop="range status" label="靶场状态"></el-table-column>
+      <el-table-column prop="number of vulnerabilities discovered" label="挖掘漏洞数量"></el-table-column>
+      <el-table-column prop="attack times" label="攻击次数"></el-table-column>
+      <el-table-column prop="effective defense times" label="有效防御次数"></el-table-column>
+      <el-table-column prop="defense ratio" label="防御比例"></el-table-column>
+      
     </el-table>
-  
-    <!-- 更新镜像 -->
-    <el-dialog v-model="editDialogVisible" title="更新镜像" width="30%">
-      <el-form
-        ref="editUserFormRef"
-        :model="editUserForm"
-        label="70px"
-        label-position="left"
-      >
-        <el-form-item label="镜像名称" prop="username">
-          <el-input v-model="editUserForm.username" />
-        </el-form-item>
-        <el-form-item label="版本号" prop="role">
-          <el-input v-model="editUserForm.role" />
-        </el-form-item>
-        <el-form-item label="资源类型" prop="loginName">
-          <el-input v-model="editUserForm.loginName" />
-        </el-form-item>
-      </el-form>
-      <template #footer>
-        <span class="dialog-footer">
-          <el-button
-            type="primary"
-            @click="closeEditDialogSubmitForm(editUserFormRef)"
-          >
-            更新
-          </el-button>
-          <el-button @click="closeEditDialogNoSubmitForm(editUserFormRef)"
-            >取消</el-button
-          >
-        </span>
-      </template>
-    </el-dialog>
   </template>
   
   <script lang="ts" setup>
@@ -252,50 +207,6 @@
     return userIndex;
   };
   
-  // edit user
-  const editUserFormRef = ref<FormInstance>();
-  const editUserForm = reactive({
-    username: "",
-    role: "",
-    loginName: "",
-  });
-  const editDialogVisible = ref(false);
-  const showEditDialog = (row: User) => {
-    editUserForm.username = row.username;
-    editUserForm.role = row.role;
-    editUserForm.loginName = row.loginName;
-    userIndex = findUserIndex(row); // update role index
-    editDialogVisible.value = true;
-  };
-  const clearEditRoleForm = () => {
-    editUserForm.username = "";
-    editUserForm.role = "";
-    editUserForm.loginName = "";
-  };
-  const clearEditDialog = (formEl: FormInstance | undefined) => {
-    if (!formEl) return;
-    formEl.resetFields();
-    clearEditRoleForm();
-  };
-  const closeEditDialogNoSubmitForm = (formEl: FormInstance | undefined) => {
-    editDialogVisible.value = false;
-    if (!formEl) return;
-    clearEditDialog(formEl);
-  };
-  const closeEditDialogSubmitForm = (formEl: FormInstance | undefined) => {
-    editDialogVisible.value = false;
-    if (!formEl) return;
-    users.value.at(userIndex)!.username = editUserForm.username;
-    users.value.at(userIndex)!.role = editUserForm.role;
-    users.value.at(userIndex)!.loginName = editUserForm.loginName;
-  };
   
-  // delete user
-  const deleteRow = (row: User) => {
-    userIndex = findUserIndex(row); // update role index
-    users.value = users.value.filter(
-      (element) => users.value.indexOf(element) !== userIndex
-    );
-  };
   </script>
   
