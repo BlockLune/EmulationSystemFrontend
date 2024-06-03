0023f3184f0e5549e1f177f9b42f704ef400eaa0
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-06-03 19:57:36 +0800

    feat: import pages for routing

--- src/main.ts
+++ src/main.ts
@@ -16,16 +16,34 @@ import "uno.css";
 import "element-plus/theme-chalk/src/message.scss";
 
 import { createMemoryHistory, createRouter } from "vue-router";
+
 import Home from "./components/pages/Home.vue";
 import SituationAwareness from "./components/pages/SituationAwareness.vue";
+import SimulationRangeMirror from "./components/pages/SimulationRange/Mirror.vue";
+import SimulationRangeContainer from "./components/pages/SimulationRange/Container.vue";
+import SimulationRangeAttackDefenseExercise from "./components/pages/SimulationRange/AttackDefenseExercise.vue";
+import SimulationRangeRange from "./components/pages/SimulationRange/Range.vue";
+import SimulationRangeDig from "./components/pages/SimulationRange/LoopHoleDig.vue";
+import SimulationRangeTask from "./components/pages/SimulationRange/Task.vue";
+import Loophole from "./components/pages/Loophole/LoopHoleController.vue";
+import SettingsRole from "./components/pages/SystemSettings/Role.vue";
+import SettingsUser from "./components/pages/SystemSettings/User.vue";
+import SettingsConfig from "./components/pages/SystemSettings/Config.vue";
+
 
 const routes = [
   { path: "/", component: Home },
   { path: "/situation_awareness", component: SituationAwareness },
-  // { path: "", component: },
-  // { path: "", component: },
-  // { path: "", component: },
-  // { path: "", component: },
+  { path: "/simulation_range/mirror", component: SimulationRangeMirror },
+  { path: "/simulation_range/container", component: SimulationRangeContainer },
+  { path: "/simulation_range/attack_defense_exercise", component: SimulationRangeAttackDefenseExercise },
+  { path: "/simulation_range/range", component: SimulationRangeRange },
+  { path: "/simulation_range/dig", component: SimulationRangeDig },
+  { path: "/simulation_range/task", component: SimulationRangeTask },
+  { path: "/loophole", component: Loophole },
+  { path: "/settings/role", component: SettingsRole },
+  { path: "/settings/user", component: SettingsUser },
+  { path: "/settings/config", component: SettingsConfig },
 ];
 const router = createRouter({
   history: createMemoryHistory(),
