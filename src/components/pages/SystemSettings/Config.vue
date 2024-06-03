031b7480e9f3f15939681fa0a85932bbbfe75180
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-06-03 17:41:24 +0800

    style: update system settings

--- src/components/pages/SystemSettings/Config.vue
+++ src/components/pages/SystemSettings/Config.vue
@@ -1,85 +1,103 @@
 <template>
-  <!-- 新增配置 -->
-  <el-button type="primary" @click="addDialogVisible = true">新增</el-button>
-  <el-dialog v-model="addDialogVisible" title="新增配置" width="30%">
-    <!-- TODO: 当前的行为是点击空白处关闭新建框会保留之前的填写记录, 是否要清除? -->
-    <el-form
-      ref="newConfigFormRef"
-      :model="newConfigForm"
-      label="70px"
-      label-position="left"
-    >
-      <el-form-item label="配置项" prop="configItem">
-        <el-input v-model="newConfigForm.configItem" placeholder="单行输入" />
-      </el-form-item>
-      <el-form-item label="配置值" prop="configValue">
-        <el-input v-model="newConfigForm.configValue" placeholder="单行输入" />
-      </el-form-item>
-      <el-form-item label="备注" prop="note">
-        <el-input
-          v-model="newConfigForm.remark"
-          placeholder="多行输入"
-          :rows="4"
-          type="textarea"
-        />
-      </el-form-item>
-    </el-form>
-    <template #footer>
-      <span class="dialog-footer">
-        <el-button
-          type="primary"
-          @click="closeAddDialogSubmitForm()"
+  <div class="flex flex-col w-full gap-2">
+    <!-- 新增配置 -->
+    <div class="flex flex-row">
+      <el-button type="primary" @click="addDialogVisible = true"
+        >新增</el-button
+      >
+      <el-dialog v-model="addDialogVisible" title="新增配置" width="30%">
+        <!-- TODO: 当前的行为是点击空白处关闭新建框会保留之前的填写记录, 是否要清除? -->
+        <el-form
+          ref="newConfigFormRef"
+          :model="newConfigForm"
+          label="70px"
+          label-position="left"
         >
-          确定
-        </el-button>
-        <el-button @click="addDialogVisible = false"
-          >取消</el-button
-        >
-      </span>
-    </template>
-  </el-dialog>
+          <el-form-item label="配置项" prop="configItem">
+            <el-input
+              v-model="newConfigForm.configItem"
+              placeholder="单行输入"
+            />
+          </el-form-item>
+          <el-form-item label="配置值" prop="configValue">
+            <el-input
+              v-model="newConfigForm.configValue"
+              placeholder="单行输入"
+            />
+          </el-form-item>
+          <el-form-item label="备注" prop="note">
+            <el-input
+              v-model="newConfigForm.remark"
+              placeholder="多行输入"
+              :rows="4"
+              type="textarea"
+            />
+          </el-form-item>
+        </el-form>
+        <template #footer>
+          <span class="dialog-footer">
+            <el-button type="primary" @click="closeAddDialogSubmitForm()">
+              确定
+            </el-button>
+            <el-button @click="addDialogVisible = false">取消</el-button>
+          </span>
+        </template>
+      </el-dialog>
+    </div>
 
-  <!-- 表格主体 -->
-  <div style="width: 100%">
-    <el-table
-        :data="configs.slice((currentPage - 1) * pageSize, currentPage * pageSize)"
-        stripe style="width: 100%">
-      <el-table-column prop="id" label="配置ID"></el-table-column>
-      <el-table-column prop="configName" label="配置项"></el-table-column>
-      <el-table-column prop="configValue" label="配置值"></el-table-column>
-      <el-table-column prop="remark" label="备注"></el-table-column>
-      <el-table-column prop="createTime" label="创建时间"></el-table-column>
-      <el-table-column prop="updateTime" label="更新时间"></el-table-column>
-      <el-table-column label="操作">
-        <template #default="{ row }">
-          <el-button link type="primary" @click="showEditDialog(row)"
-          >编辑</el-button
-          >
-          <el-popconfirm
+    <!-- 表格主体 -->
+    <div class="flex flex-col w-full gap-2">
+      <el-table
+        :data="
+          configs.slice((currentPage - 1) * pageSize, currentPage * pageSize)
+        "
+        stripe
+        style="width: 100%"
+      >
+        <el-table-column prop="id" label="配置ID"></el-table-column>
+        <el-table-column prop="configName" label="配置项"></el-table-column>
+        <el-table-column prop="configValue" label="配置值"></el-table-column>
+        <el-table-column prop="remark" label="备注"></el-table-column>
+        <el-table-column prop="createTime" label="创建时间"></el-table-column>
+        <el-table-column prop="updateTime" label="更新时间"></el-table-column>
+        <el-table-column label="操作">
+          <template #default="{ row }">
+            <el-button link type="primary" @click="showEditDialog(row)"
+              >编辑</el-button
+            >
+            <el-popconfirm
               title="确认删除？"
               confirm-button-text="确认"
               cancel-button-text="取消"
               @confirm="deleteRow(row)"
-          >
-            <template #reference>
-              <el-button link type="danger">删除</el-button>
-            </template>
-          </el-popconfirm>
-        </template>
-      </el-table-column>
-    </el-table>
-    <el-pagination
-        v-model:current-page="currentPage"
-        v-model:page-size="pageSize"
-        :page-sizes="[5, 10, 20, 30, 40]"
-        :small="small"
-        :disabled="disabled"
-        :background="background"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="configs.length"
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-    />
+            >
+              <template #reference>
+                <el-button link type="danger">删除</el-button>
+              </template>
+            </el-popconfirm>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!--分页-->
+      <div
+        class="flex flex-row justify-center items-center"
+        v-if="configs.length > pageSize"
+      >
+        <el-pagination
+          v-model:current-page="currentPage"
+          v-model:page-size="pageSize"
+          :page-sizes="[5, 10, 20, 30, 40]"
+          :small="small"
+          :disabled="disabled"
+          :background="background"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="configs.length"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+        />
+      </div>
+    </div>
   </div>
 
   <!-- 编辑角色 -->
@@ -94,7 +112,7 @@
         <el-input v-model="editConfigForm.configId" disabled />
       </el-form-item>
       <el-form-item label="配置项" prop="configItem">
-        <el-input v-model="editConfigForm.configItem"/>
+        <el-input v-model="editConfigForm.configItem" />
       </el-form-item>
       <el-form-item label="配置值" prop="configValue">
         <el-input v-model="editConfigForm.configValue" />
@@ -105,23 +123,18 @@
     </el-form>
     <template #footer>
       <span class="dialog-footer">
-        <el-button
-          type="primary"
-          @click="closeEditDialogSubmitForm"
-        >
+        <el-button type="primary" @click="closeEditDialogSubmitForm">
           确定
         </el-button>
-        <el-button @click="editDialogVisible = false"
-          >取消</el-button
-        >
+        <el-button @click="editDialogVisible = false">取消</el-button>
       </span>
     </template>
   </el-dialog>
 </template>
 
 <script lang="ts" setup>
-import {ref, reactive, onMounted} from "vue";
-import {ElMessage} from "element-plus";
+import { ref, reactive, onMounted } from "vue";
+import { ElMessage } from "element-plus";
 import axios from "axios";
 
 interface Config {
@@ -133,19 +146,19 @@ interface Config {
 }
 
 onMounted(() => {
-  listConfigs()
-})
+  listConfigs();
+});
 
 const configs = ref<Config[]>([]);
 
 const listConfigs = () => {
-  configs.value = []
+  configs.value = [];
   axios({
     headers: {
-      Authorization: localStorage.getItem('Authorization')
+      Authorization: localStorage.getItem("Authorization"),
     },
-    method: 'get',
-    url: '/system/config/selectByPage/1/10000'
+    method: "get",
+    url: "/system/config/selectByPage/1/10000",
   }).then((response) => {
     for (const config of response.data.data.list) {
       configs.value.push(config);
@@ -155,118 +168,132 @@ const listConfigs = () => {
 
 const addConfig = (configItem: string, configValue: string, remark: string) => {
   axios({
-    method: 'post',
-    url: '/system/config/createConfig',
+    method: "post",
+    url: "/system/config/createConfig",
     headers: {
-      'Authorization': localStorage.getItem('Authorization'),
+      Authorization: localStorage.getItem("Authorization"),
     },
     data: {
       configItem: configItem,
       configValue: configValue,
-      remark: remark
-    }
+      remark: remark,
+    },
   }).then((response) => {
-    ElMessage(response.data.message)
+    ElMessage(response.data.message);
   });
-}
+};
 
 const deleteConfig = (configId: string) => {
   axios({
-    method: 'post',
-    url: '/system/config/deleteConfig',
+    method: "post",
+    url: "/system/config/deleteConfig",
     headers: {
-      'Authorization': localStorage.getItem('Authorization'),
+      Authorization: localStorage.getItem("Authorization"),
     },
     data: {
-      configId: configId
-    }
+      configId: configId,
+    },
   }).then((response) => {
-    ElMessage(response.data.message)
+    ElMessage(response.data.message);
   });
-}
+};
 
-const updateConfig = (configId: string, configItem: string, configValue: string, remark: string) => {
+const updateConfig = (
+  configId: string,
+  configItem: string,
+  configValue: string,
+  remark: string
+) => {
   axios({
-    method: 'post',
-    url: '/system/config/updateConfig',
+    method: "post",
+    url: "/system/config/updateConfig",
     headers: {
-      'Authorization': localStorage.getItem('Authorization'),
+      Authorization: localStorage.getItem("Authorization"),
     },
     data: {
       configId: configId,
       configItem: configItem,
       configValue: configValue,
-      remark: remark
-    }
+      remark: remark,
+    },
   }).then((response) => {
-    ElMessage(response.data.message)
+    ElMessage(response.data.message);
   });
 };
 
 // add new config
 const newConfigForm = reactive({
-  configItem: '',
-  configValue: '',
-  remark: '',
+  configItem: "",
+  configValue: "",
+  remark: "",
 });
 
 const addDialogVisible = ref(false);
 
 const closeAddDialogSubmitForm = () => {
-  addConfig(newConfigForm.configItem, newConfigForm.configValue, newConfigForm.remark)
+  addConfig(
+    newConfigForm.configItem,
+    newConfigForm.configValue,
+    newConfigForm.remark
+  );
   window.setTimeout(() => {
-    listConfigs()
-  }, 250)
-  newConfigForm.configItem = ''
-  newConfigForm.configValue = ''
-  newConfigForm.remark = ''
-  addDialogVisible.value = false
+    listConfigs();
+  }, 250);
+  newConfigForm.configItem = "";
+  newConfigForm.configValue = "";
+  newConfigForm.remark = "";
+  addDialogVisible.value = false;
 };
 
 // edit user
 const editConfigForm = reactive({
-  configId: '',
-  configItem: '',
-  configValue: '',
-  remark: '',
+  configId: "",
+  configItem: "",
+  configValue: "",
+  remark: "",
 });
 const editDialogVisible = ref(false);
 
 const showEditDialog = (row) => {
-  editDialogVisible.value = true
-  editConfigForm.configId = row.id
-  editConfigForm.configItem = row.configName
-  editConfigForm.configValue = row.configValue
-  editConfigForm.remark = row.remark
-}
+  editDialogVisible.value = true;
+  editConfigForm.configId = row.id;
+  editConfigForm.configItem = row.configName;
+  editConfigForm.configValue = row.configValue;
+  editConfigForm.remark = row.remark;
+};
 
 const closeEditDialogSubmitForm = () => {
-  updateConfig(editConfigForm.configId, editConfigForm.configItem, editConfigForm.configValue, editConfigForm.remark)
+  updateConfig(
+    editConfigForm.configId,
+    editConfigForm.configItem,
+    editConfigForm.configValue,
+    editConfigForm.remark
+  );
   window.setTimeout(() => {
-    listConfigs()
-  }, 250)
-  editDialogVisible.value = false
+    listConfigs();
+  }, 250);
+  editDialogVisible.value = false;
 };
 
 // delete user
 const deleteRow = (row: Config) => {
-  deleteConfig(row.id)
+  deleteConfig(row.id);
   window.setTimeout(() => {
-    listConfigs()
-  }, 250)
+    listConfigs();
+  }, 250);
 };
 
-const small = ref(false)
-const background = ref(true)
-const disabled = ref(false)
-const pageSize = ref(5)
-const currentPage = ref(1)
+const small = ref(false);
+const background = ref(true);
+const disabled = ref(false);
+const pageSize = ref(5);
+const currentPage = ref(1);
 
 const handleSizeChange = (val: number) => {
-  pageSize.value = val
-}
+  pageSize.value = val;
+};
 
 const handleCurrentChange = (val: number) => {
-  currentPage.value = val
-}
+  currentPage.value = val;
+};
 </script>
--- src/components/pages/SystemSettings/Role.vue
+++ src/components/pages/SystemSettings/Role.vue
@@ -1,77 +1,111 @@
 <template>
-  <!-- 新增角色 -->
-  <el-button type="primary" @click="addDialogVisible = true">新增</el-button>
-  <el-dialog v-model="addDialogVisible" title="新增角色" width="30%">
-    <!-- TODO: 当前的行为是点击空白处关闭新建框会保留之前的填写记录, 是否要清除? -->
-    <el-form ref="newRoleFormRef" :model="newRoleForm" label="70px" label-position="left">
-      <el-form-item label="角色名称" prop="name">
-        <el-input v-model="newRoleForm.name" placeholder="单行输入" />
-      </el-form-item>
-      <el-form-item label="权限">
-        <el-radio-group v-model="newRoleForm.auth">
-          <el-radio value="仿真靶场管理">仿真靶场管理</el-radio>
-          <el-radio value="靶场管理">靶场管理</el-radio>
-          <el-radio value="容器管理">容器管理</el-radio>
-          <el-radio value="镜像管理">镜像管理</el-radio>
-          <el-radio value="攻防演练">攻防演练</el-radio>
-          <el-radio value="漏洞库管理">漏洞库管理</el-radio>
-          <el-radio value="系统管理">系统管理</el-radio>
-        </el-radio-group>
-      </el-form-item>
-    </el-form>
-    <template #footer>
-      <span class="dialog-footer">
-        <el-button type="primary" @click="closeAddDialogSubmitForm">
-          确定
-        </el-button>
-        <el-button @click="addDialogVisible = false">取消</el-button>
-      </span>
-    </template>
-  </el-dialog>
-
-  <!-- 表格主体 -->
-  <div style="width: 100%">
-    <el-table :data="roles.slice((currentPage - 1) * pageSize, currentPage * pageSize)"
-              stripe style="width: 100%">
-      <el-table-column prop="id" label="角色ID"></el-table-column>
-      <el-table-column prop="name" label="角色名称"></el-table-column>
-      <el-table-column prop="auth" label="权限">
-        <!--      <template #default="{ row }">-->
-        <!--        <span>{{ formatPermissions(row.auth) }}</span>-->
-        <!--      </template>-->
-      </el-table-column>
-      <el-table-column prop="createTime" label="创建时间"></el-table-column>
-      <el-table-column prop="updateTime" label="更新时间"></el-table-column>
-      <el-table-column label="操作">
-        <template #default="{ row }">
-          <el-button link type="primary" @click="showEditDialog(row)">编辑</el-button>
-          <el-popconfirm title="确认删除？" confirm-button-text="确认" cancel-button-text="取消" @confirm="deleteRow(row)">
-            <template #reference>
-              <el-button link type="danger">删除</el-button>
-            </template>
-          </el-popconfirm>
+  <div class="flex flex-col w-full gap-2">
+    <!-- 新增角色 -->
+    <div class="flex flex-row">
+      <el-button type="primary" @click="addDialogVisible = true"
+        >新增</el-button
+      >
+      <el-dialog v-model="addDialogVisible" title="新增角色" width="30%">
+        <!-- TODO: 当前的行为是点击空白处关闭新建框会保留之前的填写记录, 是否要清除? -->
+        <el-form
+          ref="newRoleFormRef"
+          :model="newRoleForm"
+          label="70px"
+          label-position="left"
+        >
+          <el-form-item label="角色名称" prop="name">
+            <el-input v-model="newRoleForm.name" placeholder="单行输入" />
+          </el-form-item>
+          <el-form-item label="权限">
+            <el-radio-group v-model="newRoleForm.auth">
+              <el-radio value="仿真靶场管理">仿真靶场管理</el-radio>
+              <el-radio value="靶场管理">靶场管理</el-radio>
+              <el-radio value="容器管理">容器管理</el-radio>
+              <el-radio value="镜像管理">镜像管理</el-radio>
+              <el-radio value="攻防演练">攻防演练</el-radio>
+              <el-radio value="漏洞库管理">漏洞库管理</el-radio>
+              <el-radio value="系统管理">系统管理</el-radio>
+            </el-radio-group>
+          </el-form-item>
+        </el-form>
+        <template #footer>
+          <span class="dialog-footer">
+            <el-button type="primary" @click="closeAddDialogSubmitForm">
+              确定
+            </el-button>
+            <el-button @click="addDialogVisible = false">取消</el-button>
+          </span>
         </template>
-      </el-table-column>
-    </el-table>
-    <el-pagination
-        v-model:current-page="currentPage"
-        v-model:page-size="pageSize"
-        :page-sizes="[5, 10, 20, 30, 40]"
-        :small="small"
-        :disabled="disabled"
-        :background="background"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="roles.length"
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-    />
+      </el-dialog>
+    </div>
+
+    <!-- 表格 -->
+    <div class="flex flex-col w-full gap-2">
+      <el-table
+        :data="
+          roles.slice((currentPage - 1) * pageSize, currentPage * pageSize)
+        "
+        stripe
+      >
+        <el-table-column prop="id" label="角色ID"></el-table-column>
+        <el-table-column prop="name" label="角色名称"></el-table-column>
+        <el-table-column prop="auth" label="权限">
+          <!--      <template #default="{ row }">-->
+          <!--        <span>{{ formatPermissions(row.auth) }}</span>-->
+          <!--      </template>-->
+        </el-table-column>
+        <el-table-column prop="createTime" label="创建时间"></el-table-column>
+        <el-table-column prop="updateTime" label="更新时间"></el-table-column>
+        <el-table-column label="操作">
+          <template #default="{ row }">
+            <el-button link type="primary" @click="showEditDialog(row)"
+              >编辑</el-button
+            >
+            <el-popconfirm
+              title="确认删除？"
+              confirm-button-text="确认"
+              cancel-button-text="取消"
+              @confirm="deleteRow(row)"
+            >
+              <template #reference>
+                <el-button link type="danger">删除</el-button>
+              </template>
+            </el-popconfirm>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!--分页-->
+      <div
+        class="flex flex-row justify-center items-center"
+        v-if="roles.length > pageSize"
+      >
+        <el-pagination
+          v-model:current-page="currentPage"
+          v-model:page-size="pageSize"
+          :page-sizes="[5, 10, 20, 30, 40]"
+          :small="small"
+          :disabled="disabled"
+          :background="background"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="roles.length"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+        />
+      </div>
+    </div>
   </div>
 
   <!-- 编辑角色 -->
   <el-dialog v-model="editDialogVisible" title="编辑角色" width="30%">
-    <el-form ref="editRoleFormRef" :model="editRoleForm" label="70px" label-position="left">
+    <el-form
+      ref="editRoleFormRef"
+      :model="editRoleForm"
+      label="70px"
+      label-position="left"
+    >
       <el-form-item label="角色名称" prop="name">
-        <el-input v-model="editRoleForm.name"/>
+        <el-input v-model="editRoleForm.name" />
       </el-form-item>
       <el-form-item label="权限">
         <el-radio-group v-model="editRoleForm.auth">
@@ -79,7 +113,7 @@
           <el-radio value="靶场管理">靶场管理</el-radio>
           <el-radio value="容器管理">容器管理</el-radio>
           <el-radio value="镜像管理">镜像管理</el-radio>
-<!--          <emulation-range-checkbox-group />-->
+          <!--          <emulation-range-checkbox-group />-->
           <el-radio value="攻防演练">攻防演练</el-radio>
           <el-radio value="漏洞库管理">漏洞库管理</el-radio>
           <el-radio value="系统管理">系统管理</el-radio>
@@ -98,36 +132,35 @@
 </template>
 
 <script lang="ts" setup>
-import axios from 'axios';
-import {onMounted, ref, reactive} from "vue";
-import {ElMessage, FormInstance} from "element-plus";
-import EmulationRangeCheckboxGroup from "./Role/EmulationRangeCheckboxGroup.vue";
-import JSONBIG from 'json-bigint'
+import axios from "axios";
+import { onMounted, ref, reactive } from "vue";
+import { ElMessage, FormInstance } from "element-plus";
+import JSONBIG from "json-bigint";
 
 onMounted(() => {
-  listRoles()
-})
+  listRoles();
+});
 
 axios.defaults.transformResponse = [
   function (data) {
     const json = JSONBIG({
-      storeAsString: true
-    })
-    const res = json.parse(data)
-    return res
-  }
-]
+      storeAsString: true,
+    });
+    const res = json.parse(data);
+    return res;
+  },
+];
 
-const roles = ref([])
+const roles = ref([]);
 
 const listRoles = () => {
-  roles.value = []
+  roles.value = [];
   axios({
     headers: {
-      Authorization: localStorage.getItem('Authorization')
+      Authorization: localStorage.getItem("Authorization"),
     },
-    method: 'get',
-    url: '/system/role/listRoles'
+    method: "get",
+    url: "/system/role/listRoles",
   }).then((response) => {
     for (const role of response.data.data) {
       roles.value.push(role);
@@ -137,111 +170,110 @@ const listRoles = () => {
 
 const deleteRole = (roleId: string) => {
   axios({
-    method: 'post',
-    url: '/system/role/deleteRole',
+    method: "post",
+    url: "/system/role/deleteRole",
     headers: {
-      'Authorization': localStorage.getItem('Authorization'),
+      Authorization: localStorage.getItem("Authorization"),
     },
     data: {
-      roleId: roleId
-    }
+      roleId: roleId,
+    },
   }).then((response) => {
-    ElMessage(response.data.message)
+    ElMessage(response.data.message);
   });
-}
+};
 
 const addRole = (roleName: string, auth: string) => {
   axios({
-    method: 'post',
-    url: '/system/role/createRole',
+    method: "post",
+    url: "/system/role/createRole",
     headers: {
-      'Authorization': localStorage.getItem('Authorization'),
+      Authorization: localStorage.getItem("Authorization"),
     },
     data: {
       roleName: roleName,
-      auth: auth
-    }
+      auth: auth,
+    },
   }).then((response) => {
-    ElMessage(response.data.message)
+    ElMessage(response.data.message);
   });
-}
+};
 
 const updateRole = (auth: string, roleId: number, roleName: string) => {
   axios({
-    method: 'post',
-    url: '/system/role/updateRole',
+    method: "post",
+    url: "/system/role/updateRole",
     headers: {
-      'Authorization': localStorage.getItem('Authorization'),
+      Authorization: localStorage.getItem("Authorization"),
     },
     data: {
       auth: auth,
       roleId: roleId,
-      roleName: roleName
-    }
+      roleName: roleName,
+    },
   }).then((response) => {
-    ElMessage(response.data.message)
+    ElMessage(response.data.message);
   });
 };
 
-const addDialogVisible = ref(false)
+const addDialogVisible = ref(false);
 
-const editDialogVisible = ref(false)
+const editDialogVisible = ref(false);
 
 const newRoleForm = reactive({
-  name: '',
-  auth: ''
-})
+  name: "",
+  auth: "",
+});
 
 const editRoleForm = reactive({
-  name: '',
-  auth: ''
-})
+  name: "",
+  auth: "",
+});
 
-let rowData
+let rowData;
 const showEditDialog = (row) => {
-  editDialogVisible.value = true
-  editRoleForm.name = row.name
-  editRoleForm.auth = row.auth
-  rowData = row
-}
+  editDialogVisible.value = true;
+  editRoleForm.name = row.name;
+  editRoleForm.auth = row.auth;
+  rowData = row;
+};
 
 const closeEditDialogSubmitForm = () => {
-  updateRole(editRoleForm.auth, rowData.id, editRoleForm.name)
+  updateRole(editRoleForm.auth, rowData.id, editRoleForm.name);
   window.setTimeout(() => {
-    listRoles()
-  }, 250)
-  editDialogVisible.value = false
-}
+    listRoles();
+  }, 250);
+  editDialogVisible.value = false;
+};
 
 const closeAddDialogSubmitForm = () => {
-  addRole(newRoleForm.name, newRoleForm.auth)
+  addRole(newRoleForm.name, newRoleForm.auth);
   window.setTimeout(() => {
-    listRoles()
-  }, 250)
-  newRoleForm.name = ''
-  newRoleForm.auth = ''
-  addDialogVisible.value = false
-}
+    listRoles();
+  }, 250);
+  newRoleForm.name = "";
+  newRoleForm.auth = "";
+  addDialogVisible.value = false;
+};
 
 const deleteRow = (row) => {
-  deleteRole(row.id)
+  deleteRole(row.id);
   window.setTimeout(() => {
-    listRoles()
-  }, 250)
+    listRoles();
+  }, 250);
+};
 
-}
-
-const small = ref(false)
-const background = ref(true)
-const disabled = ref(false)
-const pageSize = ref(5)
-const currentPage = ref(1)
+const small = ref(false);
+const background = ref(true);
+const disabled = ref(false);
+const pageSize = ref(5);
+const currentPage = ref(1);
 
 const handleSizeChange = (val: number) => {
-  pageSize.value = val
-}
+  pageSize.value = val;
+};
 
 const handleCurrentChange = (val: number) => {
-  currentPage.value = val
-}
+  currentPage.value = val;
+};
 </script>
