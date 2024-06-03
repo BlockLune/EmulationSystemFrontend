ccdfd7a9a6ee3a7bc9691a2ae0016ed7dceb18eb
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-06-03 17:15:56 +0800

    feat: centralize the login form

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,31 +1,33 @@
 <template>
-  <el-form
-    ref="loginFormRef"
-    :model="loginForm"
-    label-width="70px"
-    label-position="left"
-  >
-    <el-form-item>
-      <el-text>{{ EMULATION_SYSTEM_NAME }} 登录界面</el-text>
-    </el-form-item>
-    <el-form-item label="用户名" prop="username">
-      <el-input v-model="loginForm.username" placeholder="输入用户名" />
-    </el-form-item>
-    <el-form-item label="密码" prop="password">
-      <el-input
-        v-model="loginForm.password"
-        placeholder="输入密码"
-        type="password"
-        show-password
-      />
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="submitForm(loginFormRef)">
-        登录
-      </el-button>
-      <el-button @click="clearForm(loginFormRef)"> 清除 </el-button>
-    </el-form-item>
-  </el-form>
+  <div class="flex justify-center items-center w-full">
+    <el-form
+      ref="loginFormRef"
+      :model="loginForm"
+      label-width="70px"
+      label-position="left"
+    >
+      <el-form-item>
+        <el-text>{{ EMULATION_SYSTEM_NAME }} 登录界面</el-text>
+      </el-form-item>
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="loginForm.username" placeholder="输入用户名" />
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input
+          v-model="loginForm.password"
+          placeholder="输入密码"
+          type="password"
+          show-password
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm(loginFormRef)">
+          登录
+        </el-button>
+        <el-button @click="clearForm(loginFormRef)"> 清除 </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script lang="ts" setup>
