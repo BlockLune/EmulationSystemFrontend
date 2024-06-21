a9fa0baa46916f965f9f3ea88dd06eb3e1c461aa
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-06-21 15:47:48 +0800

    feat: use env file store some constants

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,8 +3,8 @@ import {
     token,
 } from "./keys";
 import axios from 'axios';
-import {ElMessage} from "element-plus";
-import {ref} from "vue";
+import { ElMessage } from "element-plus";
+import { ref } from "vue";
 
 
 axios.defaults.baseURL = baseApiUrl;
@@ -16,7 +16,6 @@ export const apikey = {
 };
 
 export const showHome = ref(localStorage.getItem('Authorization') === null);
-// export const showHome = ref(sessionStorage.getItem('Authorization') === null);
 
 // 创建一个axios实例
 const instance = axios.create({
@@ -24,42 +23,18 @@ const instance = axios.create({
     timeout: 0,
 });
 
-// // 添加请求拦截器
-// instance.interceptors.request.use(
-//     (config) => {
-//         // 在发送请求之前做些什么
-//         const token = localStorage.getItem('Authorization');
-//         if (token) {
-//             config.headers.Authorization = `Bearer ${token}`;
-//         }
-//         return config;
-//     },
-//     (error) => {
-//         // 对请求错误做些什么
-//         return Promise.reject(error);
-//     }
-// );
-
-// 添加响应拦截器
 instance.interceptors.response.use(
     (response) => {
-        // 对响应数据做些什么
-        // console.log(response.data.code)
         if (response.data.code === 401) {
-            // 如果返回401，则移除auth并重新登录
             localStorage.removeItem('Authorization');
-            // await router.push('/login');
             showHome.value = true;
         }
         return response;
     },
     async (error) => {
-        // 对响应错误做些什么
         console.log(error)
         if (error.response && error.response.status === 401) {
-            // 如果返回401，则移除auth并重新登录
             localStorage.removeItem('Authorization');
-            // await router.push('/login');
             showHome.value = true;
         }
         return Promise.reject(error);
@@ -75,8 +50,6 @@ interface Role {
     updatedAt: string,
 };
 
-//export let roless = [] as Role[];
-
 export const loginReq = (loginName: string, password: string) => {
     const postData = {
         loginName: loginName,
@@ -85,80 +58,8 @@ export const loginReq = (loginName: string, password: string) => {
     instance.post('/system/user/login', postData)
         .then((response) => {
             apikey.value = token + response.data.data;
-            // sessionStorage.setItem('Authorization', apikey.value);
             localStorage.setItem('Authorization', apikey.value);
             ElMessage(response.data.message);
             showHome.value = false;
         });
 };
-
-// export const listRoles = () => {
-//     axios.create({headers: {Authorization: localStorage.getItem('Authorization')}})
-//         .get('/system/role/listRoles')
-//         .then((response) => {
-//             roless = []
-//             for (const role of response.data.data) {
-//                 roless.push(role);
-//             }
-//         });
-// };
-
-// export const updateRole = (auth: string, roleId: number, roleName: string) => {
-//     axios({
-//         method: 'post',
-//         url: '/system/role/updateRole',
-//         headers: {
-//             'Authorization': localStorage.getItem('Authorization'),
-//         },
-//         data: {
-//             auth: auth,
-//             roleId: roleId,
-//             roleName: roleName
-//         }
-//     }).then(
-//     );
-// };
-
-// export const deleteRole = (roleId: number) => {
-//     axios({
-//         method: 'post',
-//         url: '/system/role/deleteRole',
-//         headers: {
-//             'Authorization': localStorage.getItem('Authorization'),
-//         },
-//         data: {
-//             roleId: roleId
-//         }
-//     }).then(
-//     );
-//
-// }
-
-// export const addRole = (roleName: string, auth: string) => {
-//     axios({
-//         method: 'post',
-//         url: '/system/role/createRole',
-//         headers: {
-//             'Authorization': localStorage.getItem('Authorization'),
-//         },
-//         data: {
-//             roleName: roleName,
-//             auth: auth
-//         }
-//     }).then(
-//     );
-// }
-
-// export const changeUserStatus = (status: string, userId: number) => {
-//     axios({
-//         method: 'post',
-//         url: '/system/user/changeUserStatus',
-//         headers: {
-//             'Authorization': localStorage.getItem('Authorization'),
-//         },
-//         data: {
-//             status: status,
-//             userId: userId
-//         }
-//     });
-// }
--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,4 +1,2 @@
-//export const baseApiUrl = "http://localhost:8888/";
-// export const baseApiUrl = "http://www.wuyouchachong.com/epsm/api";
-export const baseApiUrl = "http://118.195.235.81:9009";
-export const token = "Bearer ";
+export const baseApiUrl = import.meta.env.VITE_BASE_API_URL;
+export const token = import.meta.env.VITE_TOKEN;
