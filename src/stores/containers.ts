b459d27edf3e8b3e19488b5bce6cb9e6bf67d28a
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-07-12 12:03:36 +0800

    feat: store for container

---
+++ src/stores/containers.ts
@@ -0,0 +1,58 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import axiosInstance from "../utils/axiosInstance";
+import type { Container, NewContainer } from "../types";
+
+const useContainersStore = defineStore('containers', () => {
+    const containers = ref<Container[] | undefined>(undefined);
+    const selectContainersByPage = async (
+        containerName: string,
+        pageNum: string,
+        pageSize: string,
+        status: string,
+        targetName: string
+    ) => {
+        try {
+            const response = await axiosInstance.post("/container/selectByPage", {
+                containerName: containerName,
+                pageNum: pageNum,
+                pageSize: pageSize,
+                status: status,
+                targetName: targetName
+            });
+            containers.value = response.data.data.list;
+            return response;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+    const createContainer = async (newContainer: NewContainer) => {
+        try {
+            const response = await axiosInstance.post("/container/create", newContainer)
+            return response;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+    const selectContainerStatus = async (containerId: string) => {
+        try {
+            const response = await axiosInstance.post("/container/selectContainerStatus", {
+                containerId: containerId
+            });
+            return response;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+    const selectTargetImage = async () => {
+        try {
+            const response = await axiosInstance.post("/container/selectTargetImage");
+            return response;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+    return { containers, selectContainersByPage, createContainer, selectContainerStatus, selectTargetImage }
+});
+
+export default useContainersStore;
