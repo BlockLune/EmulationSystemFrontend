8798a6671dd1939b7eaa6ad35c4a86d18aefeed1
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-09-14 14:27:07 +0800

    style: optimize the header

--- src/components/layouts/BaseHeader.vue
+++ src/components/layouts/BaseHeader.vue
@@ -1,9 +1,17 @@
 <template>
   <el-menu mode="horizontal">
-    <el-menu-item>
+    <el-menu-item index="0">
       <span>{{ EMULATION_SYSTEM_NAME }}</span>
     </el-menu-item>
-    <el-menu-item>
+    <el-menu-item index="1" @click="toggleDark()">
+      <button
+        class="border-none w-full bg-transparent cursor-pointer"
+        style="height: var(--ep-menu-item-height)"
+      >
+        <i inline-flex i="dark:ep-moon ep-sunny"></i>
+      </button>
+    </el-menu-item>
+    <el-menu-item index="2">
       <el-link
         :underline="false"
         href="https://modao.cc/app/6XdQIh9Es69j5wU7F934Ol"
@@ -12,32 +20,12 @@
         <span>原型</span>
       </el-link>
     </el-menu-item>
-    <el-menu-item disabled>
-      <span>关于</span>
-    </el-menu-item>
-    <el-menu-item @click="toggleDark()">
+    <el-menu-item index="3" @click="logout()">
       <button
         class="border-none w-full bg-transparent cursor-pointer"
         style="height: var(--ep-menu-item-height)"
-      >
-        <i inline-flex i="dark:ep-moon ep-sunny" />
-      </button>
-    </el-menu-item>
-    <el-menu-item>
-      <button
-        class="border-none w-full bg-transparent cursor-pointer"
-        style="height: var(--ep-menu-item-height)"
-        @click="logout"
       >
         <span>退出登录</span>
-      </button>
-    </el-menu-item>
-    <el-menu-item v-if="showClearLocalStorageButton">
-      <button
-        class="border-none w-full bg-transparent cursor-pointer"
-        @click="clearLocalStorage"
-      >
-        Clear localStorage
       </button>
     </el-menu-item>
   </el-menu>
@@ -52,11 +40,6 @@ import { deleteStoredToken } from "~/utils/handleToken.ts";
 const { toggleDark } = useDarkStore();
 
 const EMULATION_SYSTEM_NAME = inject<string>("EMULATION_SYSTEM_NAME");
-const clearLocalStorage = () => {
-  localStorage.clear();
-  console.log("LocalStorage cleared.");
-};
-const showClearLocalStorageButton = import.meta.env.DEV;
 
 const router = useRouter();
 const logout = () => {
@@ -64,3 +47,9 @@ const logout = () => {
   router.push("/login");
 };
 </script>
+
+<style>
+.el-menu--horizontal > .el-menu-item:nth-child(1) {
+  margin-right: auto;
+}
+</style>
