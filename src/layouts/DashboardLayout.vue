35de113e3305bff9acc4dfd61d4c58766ebcca48
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-09-14 13:04:56 +0800

    fix: dashboard layout, make the side menu fixed

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,11 +1,13 @@
 <template>
   <el-config-provider namespace="ep">
-    <BaseHeader />
-    <el-container>
-      <BaseSide />
-      <el-container class="p-4">
-        <RouterView />
-      </el-container>
-    </el-container>
+    <div class="flex flex-col h-screen">
+      <BaseHeader />
+      <div class="flex flex-1 overflow-hidden">
+        <BaseSide />
+        <main class="flex-1 p-4 overflow-y-auto">
+          <RouterView />
+        </main>
+      </div>
+    </div>
   </el-config-provider>
 </template>
