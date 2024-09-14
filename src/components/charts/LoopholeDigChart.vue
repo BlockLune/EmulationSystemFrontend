aa61cf9d48671102405fce9e354fcfa6cffe921a
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-09-14 10:35:11 +0800

    feat(charts): loophole dig chart

---
+++ src/components/charts/LoopholeDigChart.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { use } from "echarts/core";
+import { BarChart } from "echarts/charts";
+import { TitleComponent, GridComponent } from "echarts/components";
+import { CanvasRenderer } from "echarts/renderers";
+import VChart, { THEME_KEY } from "vue-echarts";
+import { ref, provide } from "vue";
+
+use([TitleComponent, GridComponent, BarChart, CanvasRenderer]);
+provide(THEME_KEY, "dark");
+
+const option = ref({
+  title: {
+    text: "漏洞挖掘态势",
+  },
+  xAxis: {
+    type: "category",
+    data: ["1/16", "1/17", "1/18", "1/19", "1/20", "1/21", "1/22"],
+  },
+  yAxis: {
+    type: "value",
+  },
+  series: [
+    {
+      type: "bar",
+      data: Array.from({ length: 7 }, (_, i) =>
+        Math.floor(Math.random() * 250)
+      ), // dummy data
+    },
+  ],
+});
+</script>
+
+<template>
+  <VChart :option="option" />
+</template>
